289ffc146ecff11ca15284be0f0b396dc2aa01db
Juuso Rytilahti <juuso.b.rytilahti@utu.fi>
2024-05-30 13:30:06 +0000

    updated the Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package fi.utu.tech.ooj.exercise4;
+
+import fi.utu.tech.ooj.exercise4.exercise1.Exercise1;
+import fi.utu.tech.ooj.exercise4.exercise2.Exercise2;
+import fi.utu.tech.ooj.exercise4.exercise3.Exercise3;
+import fi.utu.tech.ooj.exercise4.exercise4.Exercise4;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Advanced Course in Object-Oriented Programming, Exercises of Part 4");
+
+        new Teht1();
+        new Teht2();
+        new Teht3();
+        new Teht4();
+    }
+}
